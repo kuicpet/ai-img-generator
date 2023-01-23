c1d532cc84ebd57759c9092cddc14b3b26edf05e
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-01-23 22:38:47 +0100

    ft--folder structure created, header added

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../assets/logo.svg'
+
+const Header = () => {
+  return (
+    <header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b-2 border-b-black'>
+      <Link to='/'>
+        <img src={Logo} alt='Logo' className='w-28 object-contain' />
+      </Link>
+      <Link
+        to='/create-post'
+        className='font-inter font-medium border-black border-2 text-black px-4 py-2 rounded-md hover:bg-black hover:text-white'>
+        Create
+      </Link>
+    </header>
+  )
+}
+
+export default Header
