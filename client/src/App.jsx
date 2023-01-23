68bd0e39c668ff42b85879b2b40b89b59c573069
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-01-23 22:44:55 +0100

    ft--routing added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import { Header } from './components'
 import { Home, CreatePost } from './pages'
 
@@ -7,7 +7,12 @@ const App = () => {
   return (
     <>
       <Header />
-      <Routes></Routes>
+      <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/create-post' element={<CreatePost />} />
+        </Routes>
+      </main>
     </>
   )
 }
