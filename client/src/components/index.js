d74a1c2ad75260cee5bd860d13c82463c3564664
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-01-23 23:45:45 +0100

    ft--Home page updated

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import Header from './Header'
+import Card from './Card'
+import FormField from './FormField'
+import Loader from './Loader'
 
-export {
-    Header
-}
+export { Header, Card, FormField, Loader }
