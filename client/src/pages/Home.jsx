cb3c2e0b2e0427bd56a54af88ff7cd7bcd4fd868
kingsley umujeyan <kingsleyumujeyan@gmail.com>
2023-01-25 12:30:53 +0100

    ft--server and client updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,6 +12,30 @@ const Home = () => {
   const [loading, setLoading] = useState(false)
   const [posts, setPosts] = useState(null)
   const [searchText, setSearchText] = useState('')
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true)
+      try {
+        const response = await fetch('http://localhost:8080/api/v1/post', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        if (response.ok) {
+          const result = await response.json()
+          setPosts(result.data.reverse())
+        }
+      } catch (error) {
+        alert(error)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchPosts()
+  }, [])
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -41,9 +65,9 @@ const Home = () => {
             )}
             <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
               {searchText ? (
-                <RenderCards data={[]} title='No search results found' /> 
-              ): (
-                <RenderCards data={[]} title='No posts found' />
+                <RenderCards data={[]} title='No search results found' />
+              ) : (
+                <RenderCards data={posts} title='No posts found' />
               )}
             </div>
           </>
